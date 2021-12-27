55f8cfb43898f6a0be61c950b8108f1066b6af05
Thomas van den Bighelaar <thomas@beardiegames.com>
2021-12-27 14:59:45 +0100

    added 3 strike evolutions system

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,6 @@
 
-pub const BLOCK: usize = 8;
-pub const MEM_SIZE: usize = BLOCK * 3;
-pub const CHAIN: usize = BLOCK * 2;
-pub const ADDR_OUT: usize = BLOCK * 2;
+pub const IN_BLOCK: usize = 8;
+pub const OUT_BLOCK: usize = 4;
+pub const MEM_SIZE: usize = IN_BLOCK + OUT_BLOCK * 2;
+pub const CHAIN: usize = IN_BLOCK + OUT_BLOCK;
+pub const ADDR_OUT: usize = IN_BLOCK + OUT_BLOCK;
--- src/lib.rs
+++ src/lib.rs
@@ -3,88 +3,57 @@ mod config;
 
 use rand::Rng;
 use rand::prelude::{ThreadRng};
-use types::{Chain, Output, Command, Opp};
+use types::*;
 use config as cfg;
 
 
-fn gen_chain(mut rng: &mut ThreadRng) -> Chain {
-    let mut chain = [Command::default(); cfg::CHAIN as usize];
-
-    for i in 0..cfg::CHAIN {
-        let mut cmd = chain[i];
-        cmd.addr_in = i as u8; // a command can only take input and cache addresses
-        cmd.addr_out = cfg::BLOCK as u8 + i as u8; // and can only write to cache or output addresses
-        cmd.opp = Opp::random(&mut rng);
-    }
-    chain
-}
-
-
 struct System {
-    mem: [bool; 24], // input: 0-7, cache: 8-15, output: 16-23
-    chain: Chain,
+    data_buffer: DataBuffer,
+    active_chain: Chain,
+    previous_chain: Chain,
+    strikes: u8,
+    archive: Vec<Chain>,
     rng: ThreadRng,
 }
 
 impl System {
     pub fn new() -> Self {
         let mut rng = rand::thread_rng();
-        System { mem: [false; 24], chain: gen_chain(&mut rng), rng, }
+
+        System { 
+            data_buffer: [false; 24], 
+            active_chain: Chain::new(&mut rng), 
+            previous_chain: Chain::new(&mut rng), 
+            strikes: 0,
+            archive: Vec::new(),
+            rng, 
+        }
     }
 
-    pub fn compute(&mut self) -> Output {
-        for cmd in &mut self.chain {
-            let addr_in = cmd.addr_in as usize;
-            let addr_out = cmd.addr_out as usize;
+    pub fn compute(&mut self) {
+        self.active_chain.execute(&mut self.data_buffer);
+    }
 
-            match cmd.opp {
-                Opp::And => self.mem[addr_out] &= self.mem[addr_in],
-                Opp::Or => self.mem[addr_out] |= self.mem[addr_in],
-                Opp::Xor => self.mem[addr_out] ^= self.mem[addr_in],
-                Opp::Not => self.mem[addr_out] = !self.mem[addr_in],
-            }
-        }
-
-        let mut output = [false; cfg::BLOCK];
-        for i in 0..cfg::BLOCK {
-            output[i] = self.mem[i + cfg::ADDR_OUT];
+    pub fn read(&self) -> Output {
+        let mut output = [false; cfg::OUT_BLOCK];
+        for i in 0..cfg::OUT_BLOCK {
+            output[i] = self.data_buffer[i + cfg::ADDR_OUT];
         }
         output
     }
 
-    pub fn apply_feedback(&mut self, failed_ouput_indexes: &[u8]) {
-        for i in failed_ouput_indexes {
-
-            // Get command with same output from chain
-            let mut failed_cmd_index = None;
+    pub fn punish(&mut self) {
+        if self.strikes < 2 { 
+            self.strikes += 1;
+        } 
+        else {
+            self.strikes = 0;
             
-            'find_cmd: for c in 0..self.chain.len() {
-                if self.chain[c].addr_out == i + cfg::ADDR_OUT as u8 {
-                    failed_cmd_index = Some(c);
-                    break 'find_cmd;
-                }
-            } 
-
-            // Change that command if found
-            if let Some(target) = failed_cmd_index {
-
-                // Decide to swap order OR change cmd opperant
-                if self.rng.gen_bool(0.5) {
-
-                    // Swap chain order
-                    // get random other chain cmd as long as it is not the target cmd
-                    let mut other =  self.rng.gen_range(0..self.chain.len());
-                    while other != target { other = self.rng.gen_range(0..self.chain.len()); }
-
-                    let cmd = self.chain[target].clone();
-                    self.chain[target] = self.chain[other].clone();
-                    self.chain[other] = cmd;
-                } 
-                else {
-                    // Change opperant
-                    self.chain[target].opp = Opp::random(&mut self.rng);
-                }
+            match self.previous_chain.age < self.active_chain.age {
+                true => self.previous_chain = self.active_chain.clone(),
+                false => self.active_chain = self.previous_chain.clone(),
             }
+            self.active_chain.evolve(&mut self.rng);
         }
     }
 }
